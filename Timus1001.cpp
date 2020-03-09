4e9530d67d93e7d63c2c8654f92f8976ee7eb798
MSAChowdhury <samselarifinchowdhury@gmail.com>
2020-03-09 21:43:08 +0600

    Timus 1001

---
+++ Timus1001.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+    int a, b,c,d;
+    float e,f,g,h;
+
+    cin>>a>>b>>c>>d;
+    e = sqrt(a);
+    f = sqrt(b);
+    g = sqrt(c);
+    h = sqrt(d);
+    cout << h <<endl;
+    cout << g <<endl;
+    cout << f <<endl;
+    cout << e <<endl;
+    return 0;
+}
