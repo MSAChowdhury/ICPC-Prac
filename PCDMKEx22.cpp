b89a43cee45ed4e70a538bd1b84141cf084ab5cd
MSAChowdhury <samselarifinchowdhury@gmail.com>
2020-03-16 23:52:24 +0600

    n! (Factorial) from the book Programming Contest by Md Mahbubul Hasan

---
+++ PCDMKEx22.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+  int n,fact = 1;
+  cin>>n;
+  for (size_t i = 1; i <=n; i++)
+  {
+    fact = fact * i;
+  }
+  cout <<fact;
+  return 0;
+}
