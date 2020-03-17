12b68ab44f4a11092175dcb572ad4b6298e42777
MSAChowdhury <samselarifinchowdhury@gmail.com>
2020-03-18 01:11:27 +0600

    Array Prac

---
+++ PCDMKArray.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  char city[10];
+
+  cin >> city;
+  int num = city[4];
+    cout << city <<"\t"<< city[4]<< "\t" << num;
+  //}
+
+  return 0;
+}
