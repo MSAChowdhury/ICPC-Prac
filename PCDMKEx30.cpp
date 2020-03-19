0b9a61e299fbbb257b44b7940354fa803eb498c3
MSAChowdhury <samselarifinchowdhury@gmail.com>
2020-03-19 19:35:52 +0600

    String length with functions

---
+++ PCDMKEx30.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstring>
+#include "cstdio"
+using namespace std;
+int main()
+{
+  char s[20];
+  int len;
+  gets(s);
+  len = strlen(s);
+  cout << "Length " <<len;
+  return 0;
+}
