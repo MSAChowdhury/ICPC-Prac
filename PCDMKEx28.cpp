2bd60fcb01cbd73c7319a7c98b8e6a1b50c60fd4
MSAChowdhury <samselarifinchowdhury@gmail.com>
2020-03-21 00:44:29 +0600

    Sorting in ascending order with vector

---
+++ PCDMKEx28.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+using namespace std;
+int main()
+{
+  vector<int> array;
+  vector<int>::iterator it;
+  array.push_back(30);
+  array.push_back(60);
+  array.push_back(40);
+  array.push_back(50);
+  array.push_back(320);
+  sort(array.begin(),array.end());
+  for (it = array.begin(); it < array.end(); it++) {
+    cout << *it <<"\t";
+  }
+
+  return 0;
+}
