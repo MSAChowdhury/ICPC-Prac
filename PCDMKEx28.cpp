d4fc3c02cdd3602c47f263052aa38acefe8d495a
MSAChowdhury <samselarifinchowdhury@gmail.com>
2020-03-21 01:19:39 +0600

    Sorting in decending order

--- PCDMKEx28.cpp
+++ PCDMKEx28.cpp
@@ -2,6 +2,12 @@
 #include <vector>
 #include <iterator>
 #include <algorithm>
+
+bool myfun(int a, int b)
+{
+  return(a > b);
+}
+
 using namespace std;
 int main()
 {
@@ -10,12 +16,12 @@ int main()
   array.push_back(30);
   array.push_back(60);
   array.push_back(40);
-  array.push_back(50);
   array.push_back(320);
-  sort(array.begin(),array.end());
-  for (it = array.begin(); it < array.end(); it++) {
+  array.push_back(50);
+
+  sort(array.begin(), array.end(),myfun);
+  for (it = array.begin(); it != array.end(); it++) {
     cout << *it <<"\t";
   }
-
   return 0;
 }
