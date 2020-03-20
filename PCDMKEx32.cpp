e42818a4410f87a3dd60a9b4e1a4fd847405ddf7
MSAChowdhury <samselarifinchowdhury@gmail.com>
2020-03-21 01:29:02 +0600

    String concatenation

---
+++ PCDMKEx32.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string.h>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+  string firstWord = "Water";
+  string secondWord = "melon";
+  string word = firstWord + secondWord;
+  
+  cout << word;
+  return 0;
+}
