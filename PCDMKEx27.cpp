fbbdb53fce3b60df05f761276c49520ec105e181
MSAChowdhury <samselarifinchowdhury@gmail.com>
2020-03-27 01:29:51 +0600

    How many 1 has in an array sequentially

---
+++ PCDMKEx27.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<cstdio>
+#include<cstring>
+using namespace std;
+int main()
+{
+  char ch[50] = "110010101010111010111001111010010011";
+  int count = 0;
+  for (size_t i = 0; ch[i] != '\0'; i+=2)
+  {
+    if(ch[i] == '1' && ch[i+1] == '1')
+    {
+      count++;
+    }
+  }
+  cout << "Total 1: "<<count<<endl;
+  return 0;
+}
