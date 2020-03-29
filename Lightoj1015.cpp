bd66a803aca04d9fcdb82e4b170fae420070fb89
MSAChowdhury <samselarifinchowdhury@gmail.com>
2020-03-30 01:16:07 +0600

    Providing total unit of dust created by N number students in T number of classes

--- Lightoj1015.cpp
+++ Lightoj1015.cpp
@@ -3,15 +3,12 @@ using namespace std;
 int solution(int n)
 {
   int num[20], case1 = 0;
-  cout <<"Enter dust unit: "<<endl;
   for (size_t j = 0; j < n; j++)
   {
     cin >> num[j];
     if(num[j] > 99) //Taking two integers, if found three place integers, it will make it 0
     {
-      cout <<"PLEASE! give only two integers"<<endl;
       num[j] = 0;
-      n++;
     }
     case1 = case1 + num[j];
   }
@@ -20,16 +17,14 @@ int solution(int n)
 int main()
 {
   int t,n,sln;
-  cout <<"Enter case: "<<endl;
   cin >> t;
   cout << endl;
 
   for (size_t i = 0; i < t; i++)
   {
-    cout <<"Enter no. of students: "<<endl;
     cin >> n;
     sln = solution(n); // calling the function again and again
-    cout << "Total dust: "<<sln<<endl;
+    cout <<sln<<endl;
   }
 
   return 0;
